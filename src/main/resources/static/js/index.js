d218286f148c902bfe1016bf61b94a3d95366c4b
zhangqian <258729416@qq.com>
2021-07-27 16:31:56 +0800

    first

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,8 +2,16 @@ new Vue({
     el: '#app',
     data: {
         selected: "1",
-        pos: 50
+        pos: 50,
+        musicStatusCss: "music-button-start"
     },
     methods: {
+        playButton: function (event) {
+            if (this.musicStatusCss == "music-button-start") {
+                this.musicStatusCss = "music-button-stop";
+            } else {
+                this.musicStatusCss = "music-button-start";
+            }
+        }
     }
 })
